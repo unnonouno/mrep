6579e479684446c7ad1072d052aa034e8826de7e
Yuya Unno <unnonouno@gmail.com>
2014-10-02 10:35:48 +0900

    Make subprocess version

--- miura/morph.py
+++ miura/morph.py
@@ -1,4 +1,15 @@
+import re
+import subprocess
+
 import MeCab
+
+def make_mecab_parser(arg=''):
+    try:
+        import MeCab
+    except ImportError:
+        return MeCabProcParser(arg)
+
+    return MeCabParser(arg)
 
 class MeCabParser(object):
     def __init__(self, arg=''):
@@ -23,4 +34,33 @@ class MeCabParser(object):
                     'feature': feature,
                 })
             node = node.next
+        return morphs
+
+class MeCabProcParser(object):
+    def __init__(self, command='mecab'):
+        self.command = command
+
+    def parse(self, s):
+        proc = subprocess.Popen(
+            self.command,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE)
+        out = proc.communicate(s)[0]
+        morphs = []
+        from cStringIO import StringIO
+        for l in StringIO(out):
+            line = l.strip()
+            if line == 'EOS':
+                break
+            m = re.match(r'(.*)\t(.*)', line)
+            if m:
+                surface = m.group(1)
+                feature = m.group(2)
+                pos = feature.split(',')[0]
+                morphs.append({
+                    'surface': surface,
+                    'pos': pos,
+                    'feature': feature,
+                })
+
         return morphs
